fb007937927a738926be51be5b1202e763e04e2d
Jochen Rau <jrau@infinitecloud.com>
2014-06-07 16:58:38 -0400

    Move route definition up

--- literacy_network/urls.py
+++ literacy_network/urls.py
@@ -15,6 +15,6 @@ urlpatterns = patterns('',
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout', {"next_page" : "/"}, name="auth_logout"),
 
     # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^admin/volunteers/+$', 'literacy_network.views.volunteers', name='volunteers'),
+    url(r'^admin/', include(admin.site.urls)),
 )
