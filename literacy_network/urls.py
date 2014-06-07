50eadb783ad63c5a367b0ce9fe00ecad63d91cf6
Jochen Rau <jrau@infinitecloud.com>
2014-06-07 14:40:44 -0400

    Redirect to volunteers

--- literacy_network/urls.py
+++ literacy_network/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic.base import RedirectView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    url(r'^(?P<volunteer_id>\d{1,10})$', 'literacy_network.views.edit_volunteer', name='home'),
+    # Examples:    url(r'^$', 'literacy_network.views.edit_volunteer', name='home'),
+    url(r'^$', RedirectView.as_view(url='volunteers', permanent=False), name='home'),
+    url(r'^volunteers/$', 'literacy_network.views.edit_volunteer', name='home'),
+    url(r'^volunteers/(?P<volunteer_id>\d{1,10})$', 'literacy_network.views.edit_volunteer', name='home'),
     # url(r'^literacy_network/', include('literacy_network.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
