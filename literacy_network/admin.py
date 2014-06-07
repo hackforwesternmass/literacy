f2154c67c8f1db073d6dcd776b8fb5a779087bfc
Jochen Rau <jrau@infinitecloud.com>
2014-06-07 13:50:36 -0400

    Remove models

--- literacy_network/admin.py
+++ literacy_network/admin.py
@@ -8,9 +8,6 @@ admin.site.register(models.VolunteerSite)
 
 admin.site.register(models.Occupation)
 
-admin.site.register(models.Visit)
-admin.site.register(models.VisitType)
-
 admin.site.register(models.Site)
 admin.site.register(models.HelpType)
 admin.site.register(models.HelpTypeResponse)
