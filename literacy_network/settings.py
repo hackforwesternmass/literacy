62ec71379f07d5b903703bc5b7927e1bb8f099db
Jochen Rau <jrau@infinitecloud.com>
2014-06-07 13:35:13 -0400

    Fix settings

--- literacy_network/settings.py
+++ literacy_network/settings.py
@@ -118,7 +118,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'literacy_network'
+    'literacy_network',
     # Uncomment the next line to enable the admin:
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
